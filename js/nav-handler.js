d3fad8b8734936513ba64ec6305a9b00ab7fe836
Wan-Teen Yang <wanteen.y@gmail.com>
2020-06-14 22:27:41 -0700

    Removed jittery scrollify plugin. Updated Index.html content

--- js/nav-handler.js
+++ js/nav-handler.js
@@ -5,44 +5,64 @@ $(document).ready(function()
 
 	//hide fixed nav and show when scrolling down the page
 	//ref: https://stackoverflow.com/questions/23315031/how-to-create-a-hidden-navbar-with-bootstrap-that-shows-after-you-scroll
-	var $fixedNav = $('.header-bar');
-
+	const $fixedNav = $('.header-bar');
 	$fixedNav.hide();
+
+	const $fixedNavHeader = document.getElementById('header');
+	const $index = document.getElementById('index');
+	var $fixedNavHeaderDisplayStyle;
+
+	const $topSection = document.getElementById('top-section');
+	const $projectsSection = document.getElementById('projects-section');
+	const $aboutSection = document.getElementById('about-section');
+	const $contactSection = document.getElementById('contact-section');
+
+	const $sections = [$topSection, $projectsSection, $aboutSection, $contactSection];
+	const $sectionIds = ['#top', '#projects', '#about', '#contact'];
+
+	const numSections = $sections.length;
+
+	const staticSectionOffset = -150;
+	var sectionOffset = 0;
 
 	$(window).scroll(function()
 	{
-		if($fixedNav.offset().top > 10) {
-			$fixedNav.fadeIn();
-		}
-		else
+		$fixedNavHeaderDisplayStyle = getComputedStyle($fixedNavHeader, null).display;
+
+		var $this = $(this);
+
+		for(i = 0; i < numSections; i++)
 		{
-			$fixedNav.fadeOut();
+			sectionOffset = i == 0 ? 0 : staticSectionOffset;
+
+			if($this.scrollTop() > ($($sections[i]).offset().top + sectionOffset) &&
+				$this.scrollTop() < ($($sections[i]).offset().top + $($sections[i]).height() + sectionOffset))
+			{
+				if(i==0)
+				{
+					removeHash();
+				}
+				else
+				{
+					if(window.location.hash == $sectionIds[i])
+						break;
+
+					window.location.hash = $sectionIds[i];
+				}
+			}
 		}
-	})
 
-
-	//fade in top section content after icon finishes drawing
-	 // $(".top-content").delay(2800).fadeIn(1000);
-
-	 //scrollify plug-in for smoother page scrolling
-	 //source: https://projects.lukehaas.me/scrollify/
-	 $.scrollify({
-	 	section:".section",
-	 	// interstitialSection : ".footer",
-	 	easing:"easeOutExpo",
-	 	// scrollbars:false,
-	 	scrollSpeed:1000,
-	 	offset:-120,
-		after:function()
-		{
-			//update fixed navigation by setting or removing 
-			//active link appearance
+		// update fixed navigation by setting or removing 
+		// 	active link appearance
 			if (window.location.hash == "#projects")
 			{
 				//update fixed nav
 				$(fixedNavProj).addClass("active");
 				$(fixedNavAbout).removeClass("active");
 				$(fixedNavContact).removeClass("active");
+
+				if ($fixedNavHeaderDisplayStyle === 'none')
+					$fixedNav.fadeIn();
 			}
 
 			else if (window.location.hash == "#about")
@@ -51,6 +71,9 @@ $(document).ready(function()
 				$(fixedNavProj).removeClass("active");
 				$(fixedNavAbout).addClass("active");
 				$(fixedNavContact).removeClass("active");
+
+				if ($fixedNavHeaderDisplayStyle === 'none')
+					$fixedNav.fadeIn();
 			}
 
 			else if (window.location.hash == "#contact")
@@ -59,17 +82,51 @@ $(document).ready(function()
 				$(fixedNavProj).removeClass("active");
 				$(fixedNavAbout).removeClass("active");
 				$(fixedNavContact).addClass("active");
+
+				if ($fixedNavHeaderDisplayStyle === 'none')
+					$fixedNav.fadeIn();
 			}
 
-			else if (window.location.hash == "#top")
+			else
 			{
 				//update fixed nav
 				$(fixedNavProj).removeClass("active");
 				$(fixedNavAbout).removeClass("active");
 				$(fixedNavContact).removeClass("active");
-			}
-		}
 
-	 });
+				if ($fixedNavHeaderDisplayStyle === 'block')
+					$fixedNav.fadeOut();
+			}
+	})
 
 });
+
+function scrollToSection(id)
+{
+	document.getElementById(id).scrollIntoView(
+	{
+		behavior: 'smooth'
+	});
+}
+
+
+function scrollToTop()
+{
+	window.scrollTo(
+	{
+		top: 0, 
+		behavior: 'smooth'
+	});
+}
+
+//Reference:
+//https://stackoverflow.com/questions/1397329/how-to-remove-the-hash-from-window-location-url-with-javascript-without-page-r/5298684#5298684
+
+function removeHash () { 
+    history.pushState(
+    	"",
+    	document.title, 
+    	window.location.pathname + window.location.search
+    );
+}
+
